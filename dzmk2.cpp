00ee7bc5442bc39988cafb995fba16e2b0eace26
Nakhaev-ALT <a.o.nakhaiev@student.khai.edu>
2024-09-22 22:28:59 +0300

    Update dzmk2.cpp

--- dzmk2.cpp
+++ dzmk2.cpp
@@ -1,65 +1,44 @@
-﻿#include <iostream>
-#include <cmath>    // для функції abs()
-#include <cstdlib>  // для system("cls")
+#include <iostream>
+#include <cmath>  // для abs()
 using namespace std;
 
-void calculateSegments() {
-    // Задача 1: Знаходимо довжини відрізків AC і BC та їх суму
-    double A, B, C;
-
-    cout << "Введіть координати A, B, C: ";
-    cin >> A >> B >> C;
-
-    double AC = abs(A - C);
-    double BC = abs(B - C);
-    double sum = AC + BC;
-
-    cout << "Довжина AC: " << AC << endl;
-    cout << "Довжина BC: " << BC << endl;
-    cout << "Сума відрізків: " << sum << endl;
-}
-
-void convertAngleToRadians() {
-    // Задача 2: Переводимо кут з градусів у радіани
-    double alpha;
-    cout << "\nВведіть кут в градусах: ";
-    cin >> alpha;
-
-    if (alpha >= 0 && alpha < 360) {
-        const double pi = 3.14159;
-        double radians = (alpha * pi) / 180.0;
-        cout << "Кут в радіанах: " << radians << endl;
-    }
-    else {
-        cout << "Помилка: кут має бути від 0 до 360!" << endl;
-    }
-}
-
 int main() {
-    char answer;
-    do {
-        system("cls");  // очищення консолі перед кожним новим запуском
+    int выбор;
+    double A, B, C, угол;
 
-        cout << "Оберіть задачу:\n";
-        cout << "1. Знаходження відрізків AC і BC\n";
-        cout << "2. Перетворення кута з градусів в радіани\n";
-        cout << "Ваш вибір (1 або 2): ";
-        int choice;
-        cin >> choice;
+    cout << "Выберите задачу:\n";
+    cout << "1. Найти отрезки AC и BC\n";
+    cout << "2. Преобразовать угол в радианы\n";
+    cout << "Ваш выбор (1 или 2): ";
+    cin >> выбор;
 
-        if (choice == 1) {
-            calculateSegments();
-        }
-        else if (choice == 2) {
-            convertAngleToRadians();
+    if (выбор == 1) {
+        cout << "Введите A, B, C: ";
+        cin >> A >> B >> C;
+
+        double AC = abs(A - C);
+        double BC = abs(B - C);
+        double сумма = AC + BC;
+
+        cout << "AC: " << AC << endl;
+        cout << "BC: " << BC << endl;
+        cout << "Сумма: " << сумма << endl;
+    }
+    else if (выбор == 2) {
+        cout << "Введите угол в градусах: ";
+        cin >> угол;
+
+        if (угол >= 0 && угол < 360) {
+            double радианы = (угол * 3.14159) / 180.0;
+            cout << "Радианы: " << радианы << endl;
         }
         else {
-            cout << "Неправильний вибір!" << endl;
+            cout << "Ошибка: угол должен быть от 0 до 360!" << endl;
         }
-
-        cout << "\nБажаєте спробувати ще раз? (y/n): ";
-        cin >> answer;
-    } while (answer == 'y' || answer == 'Y');
+    }
+    else {
+        cout << "Неправильный выбор!" << endl;
+    }
 
     return 0;
 }
